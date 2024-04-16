7adbed7ddb372cad430cd084f7e42586cc29bb50
jaseemahmed <mailjaseem.ak@gmail.com>
2024-04-16 15:13:04 +0400

    whatsapp link fix

--- src/components/FabBtn/FabBtn.js
+++ src/components/FabBtn/FabBtn.js
@@ -7,7 +7,7 @@ const FabBtn = () => {
   const classes = useStyles();
 
   const openWhatsapp = () => {
-    window.open('https://api.whatsapp.com/send?phone=+971 505683891', "blank")
+    window.open('https://api.whatsapp.com/send?phone=+971505683891', "blank")
   }
   
   return (
